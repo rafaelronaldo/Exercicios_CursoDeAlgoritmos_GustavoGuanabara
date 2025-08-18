d5c7efb981d03e4de8d26c43345d0211c13404e2
Rafael Ronaldo <rafaelronaldo.dev@gmail.com>
2025-08-17 21:40:20 -0300

    Create Exercicio1.cpp

---
+++ Exercicio1.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <locale.h>
+
+int main () {
+
+    //1) Escreva um programa que mostre na tela a mensagem "Olá, Mundo!"//
+
+    setlocale(LC_ALL, "Portuguese");
+    printf ("Olá, Mundo!");
+
+
+
+
+
+
+
+
+
+
+
+}
